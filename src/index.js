3853c2722e80ba29d23788d1d8daa2324e1e2b24
김가영 <83538598+gykim0923@users.noreply.github.com>
2022-01-16 11:50:16 +0900

    JSX

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
-const rootElement = document.getElementById("root");
+// const rootElement = document.getElementById("root");
+const name = "Josh Perez";
+const element = <h1>Hello, {name}</h1>;
 ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>,
+  element,
+  document.getElementById("root")
 );
