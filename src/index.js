80a224f6a2f13d0899eba76ae1d430364fe9f3fb
김가영 <83538598+gykim0923@users.noreply.github.com>
2022-01-16 12:16:39 +0900

    JSX2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+// import App from "./App";
 
-// const rootElement = document.getElementById("root");
-const name = "Josh Perez";
-const element = <h1>Hello, {name}</h1>;
-ReactDOM.render(
-  // <StrictMode>
-  //   <App />
-  // </StrictMode>,
-  element,
-  document.getElementById("root")
-);
+// // const rootElement = document.getElementById("root");
+// const name = "Josh Perez";
+// const element = <h1>Hello, {name}</h1>;
+// ReactDOM.render(
+//   // <StrictMode>
+//   //   <App />
+//   // </StrictMode>,
+//   element,
+//   document.getElementById("root")
+// );import { StrictMode } from "react";
+function formatName(user) {
+  return user.firstName + " " + user.lastName;
+}
+
+const user = {
+  firstName: "Harper",
+  lastName: "Perez"
+};
+
+const element = <h1>Hello, {formatName(user)}!</h1>;
+
+ReactDOM.render(element, document.getElementById("root"));
